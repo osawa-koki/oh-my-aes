b489b02a4d829e7e4fe2153e53f942107b28d892
osawa-koki <github@osawa.dev>
2023-03-05 00:02:11 +0900

    小さな修正。

--- web/setting.ts
+++ web/setting.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const setting = {
   isProd,
   basePath: Env.basePath,
-  apiPath: isProd ? '' : 'http://localhost:8080',
+  apiPath: isProd ? '' : 'http://localhost:8000',
   title: '🐸 Next.js Template 🐸',
 };
 
