b37637375c57e9bf8c517f506f2b2bbcfac02f9e
osawa-koki <github@osawa.dev>
2023-03-05 03:46:40 +0900

    小さな修正。

--- Tests/ECB.test.cs
+++ Tests/ECB.test.cs
@@ -9,7 +9,7 @@ public partial class MyTest : IClassFixture<WebApplicationFactory<Program>>
     // // テスト用のデータを生成する
     var cipher_data_list = new string[] { "HelloWorld", "あいうえお", "ｱｲｳｴｵ", "人工知能" };
     var cipher_key_list = new string[] { "ABC", "_ABC", "ｱｲｳｴｵ", "あ", "計算機" };
-    var cipher_length_list = new int[] { 256, 256, 256 };
+    var cipher_length_list = new int[] { 256 };
 
     for (int i = 0; i < 30; i++)
     {
@@ -32,7 +32,7 @@ public partial class MyTest : IClassFixture<WebApplicationFactory<Program>>
       var encrypt_result = JsonConvert.DeserializeObject<MyResponseType>(encrypt_response_string);
 
       // 復号化を行う
-      var decrypt_request_path = $"/api/cipher/aes/ecb/decrypt/{cipher_length}?key={key_string}&data={encrypt_result.Encrypted}";
+      var decrypt_request_path = $"/api/cipher/aes/ecb/decrypt/{cipher_length}?key={key_string}&data={encrypt_result!.Encrypted}";
       _testOutputHelper.WriteLine($"decrypt_request_path -> {decrypt_request_path}");
       var decrypt_response = await _client.GetAsync(decrypt_request_path);
       decrypt_response.EnsureSuccessStatusCode();
@@ -42,9 +42,39 @@ public partial class MyTest : IClassFixture<WebApplicationFactory<Program>>
       _testOutputHelper.WriteLine($"decrypt_response_string -> {decrypt_response_string}");
       var decrypt_result = JsonConvert.DeserializeObject<MyResponseType>(decrypt_response_string);
 
-      Assert.Equal(data_string, decrypt_result.Decrypted);
-      
+      Assert.Equal(data_string, decrypt_result!.Decrypted);
+
       _testOutputHelper.WriteLine($"========== ========= ========== ========= ==========");
+    }
+  }
+
+  [Fact]
+  public async Task ECB_size_valider()
+  {
+    {
+      var encrypt_request_path = $"/api/cipher/aes/ecb/encrypt/{100}?key=HelloWorld&data=HelloWorld";
+      var response = await _client.GetAsync(encrypt_request_path);
+      Assert.Equal(400, (int)response.StatusCode);
+    }
+    {
+      var encrypt_request_path = $"/api/cipher/aes/ecb/encrypt/{128}?key=HelloWorld&data=HelloWorld";
+      var response = await _client.GetAsync(encrypt_request_path);
+      Assert.Equal(200, (int)response.StatusCode);
+    }
+    {
+      var encrypt_request_path = $"/api/cipher/aes/ecb/encrypt/{192}?key=HelloWorld&data=HelloWorld";
+      var response = await _client.GetAsync(encrypt_request_path);
+      Assert.Equal(200, (int)response.StatusCode);
+    }
+    {
+      var encrypt_request_path = $"/api/cipher/aes/ecb/encrypt/{256}?key=HelloWorld&data=HelloWorld";
+      var response = await _client.GetAsync(encrypt_request_path);
+      Assert.Equal(200, (int)response.StatusCode);
+    }
+    {
+      var encrypt_request_path = $"/api/cipher/aes/ecb/encrypt/{512}?key=HelloWorld&data=HelloWorld";
+      var response = await _client.GetAsync(encrypt_request_path);
+      Assert.Equal(400, (int)response.StatusCode);
     }
   }
 }
